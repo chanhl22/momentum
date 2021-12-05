17b71072f882b2e1bc6efb231f97d75c2ba2d789
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-05 16:26:17 +0900

    오류 수정

---
+++ js/background.js
@@ -0,0 +1,12 @@
+const images = [
+    "0.jpeg",
+    "1.jpeg",
+    "2.jpeg",
+    "3.jpeg",
+    "4.jpeg",
+    "5.jpeg",
+]
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+console.log(chosenImage);
