99c14ac9f64cab7dd53f356f637fe8ac8a5ccf53
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-07 18:15:27 +0900

    Update Logout

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const logout = document.querySelector("#logout-button");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -16,6 +17,7 @@ function onLoginSubmit(event) {
  function paintGreetings(username) {
     greeting.classList.remove(HIDDEN_CLASSNAME);
     greeting.innerText = `Hello! ${username}`;
+    logout.classList.remove(HIDDEN_CLASSNAME);
  }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -26,3 +28,12 @@ if (savedUsername === null) {
 } else {
     paintGreetings(savedUsername);
 }
+
+function onLogout() {
+    if(confirm('Do you want to Logout?')) {
+        localStorage.clear();
+        location.reload();
+    }
+}
+
+logout.addEventListener("click", onLogout);
