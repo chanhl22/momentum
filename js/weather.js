85079149745f274b2b8e53a6c22a79eaa696ee31
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-07 17:06:29 +0900

    Add Weather

--- js/weather.js
+++ js/weather.js
@@ -5,10 +5,10 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-        city.innerText = data.name;
+        const weather = document.querySelector("#weather span:last-child");
+        const city = document.querySelector("#weather span:first-child");
+        city.innerText = `${data.name} / `;
+        weather.innerText = `${Math.round(data.main.temp)}°C ${data.weather[0].main}`;
     });
 }
 function onGeoError() {
